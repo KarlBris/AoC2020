ed27b2d5088d13ee1fffd74d0b771ce412bbe941
Karl Bristav <karl.bristav@gmail.com>
2020-12-01 06:27:18 +0100

    complete Day 1

--- Day1.hs
+++ Day1.hs
@@ -3,7 +3,11 @@ module Day1 where
 import Testrunner
 
 part1 :: String -> Integer
-part1 i = read i
+part1 i = head productList
+  where es = map read $ lines i
+        productList = [a * b | a <- es, b <- es, a + b == 2020]
 
 part2 :: String -> Integer
-part2 i = undefined
+part2 i = head productList
+  where es = map read $ lines i
+        productList = [a * b * c | a <- es, b <- es, c <- es, a + b + c == 2020]
--- Template.hs
+++ Template.hs
@@ -7,3 +7,13 @@ part1 i = read i
 
 part2 :: String -> Integer
 part2 i = undefined
+
+
+
+
+
+
+test1 = compareToExampleSolutions part1 examples? solutions?_1
+test2 = compareToExampleSolutions part2 examples? solutions?_2
+go1   = part1 input?
+go2   = part2 input?
