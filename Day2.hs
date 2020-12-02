c5c4c48eb670013f42b0af390bf85c4ffe59b549
Karl Bristav <karl.bristav@gmail.com>
2020-12-02 06:57:55 +0100

    add day 2

---
+++ Day2.hs
@@ -0,0 +1,37 @@
+module Day2 where
+
+import Testrunner
+
+part1 :: String -> Integer
+part1 i = toInteger $ length $ filter (==True) $ map isPasswordValid (splitStuff i) 
+
+splitStuff :: String -> [(((String, String),Char),String)]
+splitStuff i = split''
+  where passLines = lines i
+        split = map (splitString ':' []) passLines
+        split' = map (\(l, r) -> (splitString ' ' [] l, tail r)) split
+        split'' = map (\((ll, lr),r) -> (((splitString '-' [] ll),head lr), r)) split'
+
+isPasswordValid :: (((String, String),Char),String) -> Bool
+isPasswordValid (((min, max), c), pass) = occurrences >= (read min) && occurrences <= (read max)
+  where occurrences = length $ filter (==c) pass
+
+part2 :: String -> Integer
+part2 i = toInteger $ length $ filter (==True) $ map isPasswordValid2 (splitStuff i) 
+
+isPasswordValid2 :: (((String, String),Char),String) -> Bool
+isPasswordValid2 (((a, b), c), pass) = (pass!!((read a)-1) == c) `xor` (pass!!((read b)-1) == c)
+
+xor True False = True
+xor False True = True
+xor _ _        = False
+
+splitString c ls (r:rs)
+  | r == c = (reverse ls, rs)
+  | otherwise = splitString c (r:ls) rs 
+splitString c ls []       = (ls, [])
+
+test1 = compareToExampleSolutions part1 examples2 solutions2_1
+test2 = compareToExampleSolutions part2 examples2 solutions2_2
+go1   = part1 input2
+go2   = part2 input2
